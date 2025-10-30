abb7704f3666c476168ed092c35e87324bfcd2c4
oliverwunder20-sys <oliverwunder20@gmail.com>
2025-10-30 19:05:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,27 @@
-// Einfacher Offline-Cache
-const CACHE = "aescherer-planner-v1";
+// sw.js – simpler App-Cache
+const CACHE_NAME = 'aescherer-planer-v1';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
 });
 
-self.addEventListener("fetch", (e) => {
-  const req = e.request;
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE).then(c => c.put(req, copy));
-      return res;
-    }).catch(() => caches.match("./index.html")))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
